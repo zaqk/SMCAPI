815752b38df680e07a700230f15d6aae45a24850
zaqk <zachking1218@gmail.com>
2015-12-30 23:01:54 -0600

    added pg dump and formatting

--- src/main/java/com/smcapi/main/Main.java
+++ src/main/java/com/smcapi/main/Main.java
@@ -9,7 +9,7 @@ import javax.sql.DataSource;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
@@ -19,28 +19,27 @@ public class Main {
 	private static final String TABLE_NAME = "main";
 	private static final String DB_CONTEXT_BEAN = "src/main/webapp/WEB-INF/conf/db-context.xml";
 
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		String webappDirLocation = "src/main/webapp";
 
-        String webPort = System.getenv("PORT");
-        if (webPort == null || webPort.isEmpty()) {
-            webPort = "8080";
-        }
+		String webPort = System.getenv("PORT");
+		if (webPort == null || webPort.isEmpty()) {
+			webPort = "8080";
+		}
 
-        Server server = new Server(Integer.valueOf(webPort));
-        WebAppContext root = new WebAppContext();
+		Server server = new Server(Integer.valueOf(webPort));
+		WebAppContext root = new WebAppContext();
 
-        validateDB();
-        
-        root.setDescriptor(webappDirLocation + "/WEB-INF/web.xml");
-        root.setResourceBase(webappDirLocation);
+		validateDB();
 
-        root.setParentLoaderPriority(true);
-        
-        server.setHandler(root);
-        server.start();
-        server.join();
+		root.setDescriptor(webappDirLocation + "/WEB-INF/web.xml");
+		root.setResourceBase(webappDirLocation);
 
+		root.setParentLoaderPriority(true);
+
+		server.setHandler(root);
+		server.start();
+		server.join();
 
 	}
 	
@@ -48,22 +47,21 @@ public class Main {
     /**
      * Attempts to connect to the DB and ensures that the necessary tables exist. Calls System.exit if there is a problem connecting to the DB
      */
-    private static void validateDB() {
-        log.info("Validating DB");
-        try {
-        	ApplicationContext ac = new FileSystemXmlApplicationContext(DB_CONTEXT_BEAN);
-        	DataSource dataSource = (DataSource) ac.getBean("dataSource");
-        	Connection conn = dataSource.getConnection();
-            //Connection conn = DB.getConnection();
-            Statement statement = conn.createStatement();
-            ResultSet rs = statement.executeQuery("select count(*) from " + TABLE_NAME);
-            rs.next();
-            long count = rs.getLong(1);
-            log.info("Successfully connected to DB, counted " + count + " rows in " + TABLE_NAME);
-        } catch (Exception e) {
-            log.severe("Exception validating DB! " + e);
-            System.exit(1);
-        }
-    }
+	private static void validateDB() {
+		log.info("Validating DB");
+		try {
+			ApplicationContext ac = new FileSystemXmlApplicationContext(DB_CONTEXT_BEAN);
+			DataSource dataSource = (DataSource) ac.getBean("dataSource");
+			Connection conn = dataSource.getConnection();
+			Statement statement = conn.createStatement();
+			ResultSet rs = statement.executeQuery("select count(*) from " + TABLE_NAME);
+			rs.next();
+			long count = rs.getLong(1);
+			log.info("Successfully connected to DB, counted " + count + " rows in " + TABLE_NAME);
+		} catch (Exception e) {
+			log.severe("Exception validating DB! " + e);
+			System.exit(1);
+		}
+	}
 
 }
