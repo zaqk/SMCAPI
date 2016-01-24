08d93f73e6e7ab06a50607108bcd8f88d13be446
zaqk <zachking1218@gmail.com>
2016-01-24 00:54:56 -0600

    Added clarification to VideoIdExtractor

--- src/main/java/com/smcapi/util/VideoIdExtractor.java
+++ src/main/java/com/smcapi/util/VideoIdExtractor.java
@@ -3,8 +3,15 @@ package com.smcapi.util;
 public class VideoIdExtractor {
 		
 		private String id;
-		
+		/**
+		 * @param the youtube url
+		 * Example: https://www.youtube.com/watch?v=QRv2_zAPrhc
+		 * Note:this method does not work on shortened youtu.be urls
+		 * 
+		 */
 		public String getVideoId(String link){
+			
+			
 			
 			for(int i = 0, n = link.length() ; i < n ; i++) { 
 			    char c = link.charAt(i);
