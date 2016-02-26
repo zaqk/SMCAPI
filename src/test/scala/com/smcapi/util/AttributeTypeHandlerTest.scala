7c0941d4aa1d9a6dcf746bd2446a506bf365bcb3
zaqk <zachking1218@gmail.com>
2016-02-25 22:39:37 -0600

    unit test for AttributeTypeHandler

---
+++ src/test/scala/com/smcapi/util/AttributeTypeHandlerTest.scala
@@ -0,0 +1,21 @@
+package com.smcapi.util
+
+import org.scalatest._
+import org.scalamock.scalatest.MockFactory
+
+import org.mockito.Mockito._
+
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+class AttributeTypeHandlerTest extends FunSuite with BeforeAndAfter with ShouldMatchers{
+  
+  test("getNullableResult() :: validate"){
+    
+    
+  }
+  
+}
